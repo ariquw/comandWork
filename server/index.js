b4a06b9bd8da6470047bfe69c70e930800186e1d
Aliona596 <alenatsarapina@yandex.ru>
2026-05-10 01:18:38 +0300

    базовое хранилище

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
+const store = require('./store');
 
 const app = express();
 const server = http.createServer(app);
@@ -15,12 +16,21 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res) {
   res.json({ status: 'Server is running' });
+});
+
+io.on('connection', function(socket) {
+  console.log('Пользователь подключился:', socket.id);
+
+  socket.on('disconnect', function() {
+    console.log('Пользователь отключился:', socket.id);
+  });
 });
 
 const PORT = 3000;
 
-server.listen(PORT, () => {
-  console.log('Server running on port', PORT);
+server.listen(PORT, function() {
+  console.log('Сервер запущен на порту', PORT);
+  console.log('WebSocket готов к работе');
 });
